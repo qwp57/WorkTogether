126a541d9173dc79a06dd8808ea19beaa52350b1
seo young choi <csy4723@gmail.com>
2022-06-06 18:20:24 +0900

    l

--- src/main/java/com/uni/wt/HomeController.java
+++ src/main/java/com/uni/wt/HomeController.java
@@ -4,6 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -32,6 +34,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
 		
 		return "index";
 	}
