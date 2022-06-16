e6ff25711b6f261366066ce488a6b3270f21266f
seo young choi <csy4723@gmail.com>
2022-06-16 15:33:48 +0900

    ㄴ

--- src/main/java/com/uni/wt/workState/controller/WorkStateController.java
+++ src/main/java/com/uni/wt/workState/controller/WorkStateController.java
@@ -65,6 +65,38 @@ public class WorkStateController {
 		return mv;
 	}
 	
+	@ResponseBody
+	@RequestMapping(value="workStateSelectWeek.do")
+	public ModelAndView workStateSelectWeek(HttpServletRequest request, ModelAndView mv) throws Exception {
+		Employee emp = (Employee) request.getSession().getAttribute("loginEmp");
+		log.info("[현재 로그인한 유저]  : {}", emp.toString());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		
+		
+		Calendar c = Calendar.getInstance();//오늘 날짜 
+		c.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);//요일을 일요일로 설정
+		String startday = sdf.format(c.getTime());
+		
+		
+		log.info("선택 주 일요일 : {}", startday);
+		
+		ArrayList<String> weekHoliday = (ArrayList<String>) lunarCal.WeekHoliday(startday);
+		log.info("[선택 주 휴일] : {} ", weekHoliday.toString());
+		Map<Spring, Object> resultMap = wsService.selectMyWork(startday, emp.getEmp_no(), weekHoliday);
+		ArrayList<Integer> workTime = wsService.selectWorkTimeList(startday, emp.getEmp_no());
+		
+		log.info("[선택 주 근로시간] : {}",resultMap.toString());//{LEFTTIME=25, TOTALTIME=27, OVERTIME=-25}
+		log.info("일주일 근로시간 리스트 : {}" , workTime.toString());
+		
+		mv.setViewName("workstate/myWorkState");
+		mv.addObject("weekWork", resultMap);
+		mv.addObject("holiday", weekHoliday);
+		mv.addObject("workTime", workTime);
+		
+		
+		return mv;
+	}
+	
 	@RequestMapping("teamWorkState.do")
 	public String teamWorkState() {
 		
